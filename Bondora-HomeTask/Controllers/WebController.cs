8cba9fa7c6cc8483d34ffd30959935ce7707667d
juluvanahd <juluvanahd@users.noreply.github.com>
2019-08-16 17:03:20 +0300

    design added

---
+++ Bondora-HomeTask/Controllers/WebController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bondora_HomeTask.Controllers
+{
+    public class WebController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
