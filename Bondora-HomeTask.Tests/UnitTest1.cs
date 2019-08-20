b4ef304ad5a19bdfd9bca679b51b9e435b7efb3b
juluvanahd <juluvanahd@users.noreply.github.com>
2019-08-20 16:10:31 +0300

    first test added

---
+++ Bondora-HomeTask.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Bondora_HomeTask.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bondora_HomeTask.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RentHeavyProduct()
+        {
+            var xd = PriceCalculation.EquipmentPrice(Types.Heavy, 5);
+            Assert.AreEqual(400, xd);
+        }
+    }
+}
